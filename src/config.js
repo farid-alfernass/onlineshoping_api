b61eaa75bb90352faa75e3363a983a62b0bf5fe7
farid.wicak <farid.wicak@gmail.com>
2018-07-10 21:38:19 +0700

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV
 const common = {
   port: 3000,
-  jwtSecret: 'pGDJVfaam11d94RT2sZyKxWw0YyPeQxJ'
+  jwtSecret: 'Z7pp7Lgn5FEKeX2QfGOqwe0k0uCZNX9RLuW7yzAL'
 }
 const config = {
   development: {
--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json({limit: '50mb'}))
 
-app.use('/api', routes)
+app.use('/', routes)
 
 app.use(errorHandle)
 
